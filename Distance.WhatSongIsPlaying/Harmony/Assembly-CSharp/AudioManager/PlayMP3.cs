e2ba905f1e5ecc3f51e02aeb52aa3b773091e3b0
Tribow <58997765+Tribow@users.noreply.github.com>
2022-09-04 23:36:47 -0400

    v1.0

---
+++ Distance.WhatSongIsPlaying/Harmony/Assembly-CSharp/AudioManager/PlayMP3.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using TagLib;
+
+namespace Distance.WhatSongIsPlaying.Harmony
+{
+    [HarmonyPatch(typeof(AudioManager), "PlayMP3")]
+    internal class AudioManager__PlayMP3
+    {
+        [HarmonyPostfix]
+        internal static void GetSongPostfix(AudioManager __instance)
+        {
+            Mod.Instance.Logger.Debug(__instance.currentCustomSongPath_);
+
+            var tfile = File.Create(__instance.currentCustomSongPath_);
+            Mod.Instance.songTitle = tfile.Tag.Title;
+            Mod.Instance.songArtist = tfile.Tag.FirstPerformer;
+            Mod.Instance.songAlbumArtist = tfile.Tag.FirstAlbumArtist;
+            tfile.Dispose();
+        }
+    }
+}
---
+++ Distance.WhatSongIsPlaying/Harmony/Assembly-CSharp/TimeWidget/PrintText.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+
+namespace Distance.WhatSongIsPlaying.Harmony
+{
+    [HarmonyPatch(typeof(TimeWidget), "PrintText")]
+    internal class TimeWidget__PrintText
+    {
+        [HarmonyPostfix]
+        internal static void ReplaceTimeWithSong(TimeWidget __instance)
+        {
+            if(Mod.Instance.Config.DisplaySongOnTimer)
+            {
+                string artistString = string.Empty;
+                if (Mod.Instance.Config.ShowAlbumArtist)
+                    artistString = Mod.Instance.songAlbumArtist + " - ";
+                else
+                    artistString = Mod.Instance.songArtist + " - ";
+
+                string songDisplayText = artistString + Mod.Instance.songTitle;
+                //If songDisplayText is longer than 14 characters, try to shorten it.
+                //If it succeeds in shortening it, display the text. If it fails it will do nothing.
+                //If it's already short enough it won't bother and display the text
+                if(songDisplayText.Length > 14)
+                {
+                   songDisplayText = Mod.Instance.songTitle;
+                   if(songDisplayText.Length <= 14)
+                    {
+                        __instance.s_.Clear();
+                        __instance.s_.Append(songDisplayText);
+                        __instance.textMesh_.text = __instance.s_.ToString();
+                    }
+                }
+                else
+                {
+                    __instance.s_.Clear();
+                    __instance.s_.Append(songDisplayText);
+                    __instance.textMesh_.text = __instance.s_.ToString();
+                }
+            }
+        }
+    }
+}
